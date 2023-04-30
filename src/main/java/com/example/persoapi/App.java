c014cd8059455e3f4b4d3d464dd99a443039ce2c
louis <louisdosnon@gmail.com>
2023-04-30 20:37:59 +0200

    rename PersoApiApplication => App

--- src/main/java/com/example/persoapi/App.java
+++ src/main/java/com/example/persoapi/App.java
@@ -5,24 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/")
-public class PersoApiApplication extends SpringBootServletInitializer {
+public class App  extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(PersoApiApplication.class);
+        return builder.sources(App.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(PersoApiApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
-
-    @GetMapping
-    public String getConcerts() {
-        return "hello from perso Api";
-    }
-
 }
