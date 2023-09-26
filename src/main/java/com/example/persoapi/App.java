74d90beda2588a0c7c0980e2602fcb8d5d2b2c48
louis <louisdosnon@gmail.com>
2023-09-26 17:56:11 +0200

    ajout dockerfile

--- src/main/java/com/example/persoapi/App.java
+++ src/main/java/com/example/persoapi/App.java
@@ -2,20 +2,9 @@ package com.example.persoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class App  extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(App.class);
-    }
-
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
